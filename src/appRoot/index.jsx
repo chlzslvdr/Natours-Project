8dc6156f5ff633a4441515b3a6010e52c69bfb76
Chelzea Salvador <chelzeasalvador@gmail.com>
2020-04-15 14:16:56 +0800

    delete useless codes

--- src/appRoot/index.jsx
+++ src/appRoot/index.jsx
@@ -1,27 +1,11 @@
-import React, { Suspense } from 'react';
-import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
-import publicRoutes from './routes/public';
-import privateRoutes from './routes/private';
-import Loading from '../components/Loading';
+import React from 'react';
+import Home from '../modules/Home';
 
 const AppRoot = () => {
-  const routes = [...publicRoutes, ...privateRoutes];
-
   return (
-    <BrowserRouter>
-      <Suspense fallback={<Loading />}>
-        <Switch>
-          <Redirect from="/" to="/home" exact />
-          {routes.map((route, index) => (
-            <Route
-              path={route.path}
-              component={route.component}
-              key={String(index)}
-            />
-          ))}
-        </Switch>
-      </Suspense>
-    </BrowserRouter>
+    <section>
+      <Home />
+    </section>
   );
 };
 
